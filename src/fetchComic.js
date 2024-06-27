0f0006766be2631e7a4a0a1278dcb92b388b0859
ob0china <143781393+sasaSilver@users.noreply.github.com>
2024-06-28 00:58:25 +0300

    Add files via upload

---
+++ src/fetchComic.js
@@ -0,0 +1,49 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function fetchComicId(email) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const searchParams = new URLSearchParams({
+            email: email
+        });
+        const response = yield fetch("https://fwd.innopolis.university/api/hw2?" + searchParams);
+        const comicId = yield response.json();
+        return comicId;
+    });
+}
+function fetchComicData(email) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const comicId = yield fetchComicId(email);
+        const searchParams = new URLSearchParams({
+            id: `${comicId}`
+        });
+        const comicData = yield fetch("https://fwd.innopolis.university/api/comic?" + searchParams);
+        return comicData.json();
+    });
+}
+function displayComic(email) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const comicData = yield fetchComicData(email);
+        const imgElement = document.createElement("img");
+        imgElement.src = comicData.img;
+        imgElement.alt = comicData.alt;
+        const titleElement = document.createElement("h1");
+        titleElement.textContent = comicData.safe_title;
+        const imgDate = new Date(comicData.year, comicData.month, comicData.day);
+        const dateElement = document.createElement("p");
+        dateElement.textContent = imgDate.toLocaleDateString();
+        const comicContainer = document.createElement("div");
+        comicContainer.appendChild(imgElement);
+        comicContainer.appendChild(titleElement);
+        comicContainer.appendChild(dateElement);
+        document.body.appendChild(comicContainer);
+    });
+}
+const email = "al.mikhailov@innopolis.university";
+displayComic(email);
---
+++ src/fetchComic.ts
@@ -0,0 +1,46 @@
+interface ComicResponse {
+    img: string;
+    alt: string;
+    safe_title: string;
+    day: number;
+    month: number;
+    year: number;
+}
+
+async function fetchComicId(email: string): Promise<string> {
+    const searchParams : URLSearchParams = new URLSearchParams({
+        email: email
+    });
+    const response: Response = await fetch("https://fwd.innopolis.university/api/hw2?" + searchParams);
+    const comicId: string = await response.json();
+    return comicId;
+}
+
+async function fetchComicData(email: string): Promise<ComicResponse> {
+    const comicId: string = await fetchComicId(email);
+    const searchParams = new URLSearchParams({
+        id: comicId
+    });
+    const comicData: Response = await fetch("https://fwd.innopolis.university/api/comic?" + searchParams);
+    return comicData.json();
+}
+
+async function displayComic(email: string): Promise<void> {
+    const comicData: ComicResponse = await fetchComicData(email);
+    const imgElement: HTMLImageElement = document.createElement("img");
+    imgElement.src = comicData.img;
+    imgElement.alt = comicData.alt;
+    const titleElement: HTMLHeadingElement = document.createElement("h1");
+    titleElement.textContent = comicData.safe_title;
+    const imgDate: Date = new Date(comicData.year, comicData.month, comicData.day);
+    const dateElement: HTMLParagraphElement = document.createElement("p");
+    dateElement.textContent = imgDate.toLocaleDateString();
+    const comicContainer: HTMLDivElement = document.createElement("div");
+    comicContainer.appendChild(imgElement);
+    comicContainer.appendChild(titleElement);
+    comicContainer.appendChild(dateElement);
+    document.body.appendChild(comicContainer);
+}
+
+const email: string = "al.mikhailov@innopolis.university";
+displayComic(email);
